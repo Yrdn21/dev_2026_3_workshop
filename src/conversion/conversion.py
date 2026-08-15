094708105b3186370502d0ab2c30624d0f3e12b8
Yrdn21 <ytordecilla54@ucatolica.edu.co>
2026-08-14 19:12:47 -0500

    Conversion de romano a numero decimal, con for y asignacion, con len,  += y-=

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -143,7 +143,21 @@ class Conversion:
             romano_a_decimal("IX") -> 9
             romano_a_decimal("MCMXCIV") -> 1994
         """
-        pass
+        numerosromanos = {"I":1, "V":5, "X":10, "L":50, "C":100, "D": 500, "M":1000}
+        resultado = 0
+        for i in range (len(romano)):
+            numeroActual = numerosromanos[romano[i]]
+            
+            if i + 1 < len(romano):
+                siguienteNumero = numerosromanos[romano[i+1]]
+                
+                if numeroActual < siguienteNumero:
+                    resultado -= numeroActual
+                else:
+                    resultado += numeroActual
+            else: 
+                resultado  += numeroActual
+        return resultado
     
     def texto_a_morse(self, texto):
         """
