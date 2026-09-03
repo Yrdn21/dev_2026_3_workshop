73653a28f12afab78c8a73f1637b9dcf42317184
Yrdn21 <ytordecilla54@ucatolica.edu.co>
2026-09-02 21:41:04 -0500

    Encontrar el numero faltante

--- src/data/data.py
+++ src/data/data.py
@@ -111,7 +111,10 @@ class Data:
         Returns:
             int: El número que falta en la secuencia
         """
-        pass
+        n = len(lista) + 1
+        suma_esperada = n * (n+1) // 2
+        suma_real = suma(lista)
+        return suma_esperada - suma_real
     
     def es_subconjunto(self, conjunto1, conjunto2):
         """
